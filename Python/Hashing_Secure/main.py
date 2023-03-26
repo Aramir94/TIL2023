b504f0806c6b991cb5a1ab8bd295010ab775e147
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2023-03-26 09:56:35 +0900

    store password to database

--- Python/Hashing_Secure/main.py
+++ Python/Hashing_Secure/main.py
@@ -15,3 +15,18 @@ h = hashlib.new("SHA256")
 h.update(b"Hello World!") 
 #print(h.digest())
 print(h.hexdigest())
+
+## What about Password to database 
+correct_password = "Mypassword1234"
+h = hashlib.new("SHA256") # new로 새로 만들어 줘야한다
+h.update(correct_password.encode())
+password_hash = h.hexdigest()
+print(password_hash)
+
+user_input = "Mypassword1234"
+h = hashlib.new("SHA256") # new로 새로 만들어 줘야한다
+h.update(user_input.encode())
+user_password_hash = h.hexdigest()
+print(user_password_hash)
+
+print(password_hash ==user_password_hash)
