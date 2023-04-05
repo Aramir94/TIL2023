f969c5410a1d0e25d866f3fed3cd5acb0c238dca
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2023-04-05 18:38:22 +0900

    printfscanf

---
+++ C_lang/nado_coding_c/printfscanf.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+/* 안녕하셈 */
+// 반갑셈
+int main(void)
+{
+    // 변수 -> 변경 가능한수 
+    // 정수형
+    int age = 12;
+    printf("%d\n", age);
+
+    age = 13;
+    printf("%d\n", age);
+
+    // 실수형
+    float f = 46.5f;
+    printf("%f\n", f);
+    printf("%.2f\n", f);
+    
+    double d = 4.526;
+    printf("%f\n", d);
+    printf("%.2f\n", d);
+    return 0;
+
+
+    // 상수 : 변하지 않는 수 
+
+}
