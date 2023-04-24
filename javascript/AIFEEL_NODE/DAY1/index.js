fb4b854070418e7e96b82d27aff729eb1fdf548f
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2023-04-24 21:54:57 +0900

    npm install 은 짜릿해

--- javascript/AIFEEL_NODE/DAY1/index.js
+++ javascript/AIFEEL_NODE/DAY1/index.js
@@ -10,3 +10,12 @@ console.log(
   "현재 날짜 시간 정보를 출력합니다",
   moment(Date.now()).format("YYYY-MM-DD HH:mm:ss")
 );
+
+/*
+npm install moment dotenv
+npm install (npm i)
+npm install nodemon --save-dev
+npm install -global <package> (npm i -g <package>)
+whereis npm 
+npm: /Users/flycode77/.nvm/versions/node/v16.16.0/bin/npm : 여기에 설치됨 
+*/
