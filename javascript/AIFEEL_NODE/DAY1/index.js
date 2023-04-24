d19fc0ae011d9e95486d94b772c3aa6b48e30c51
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2023-04-24 21:16:58 +0900

    package는 짜릿해

--- javascript/AIFEEL_NODE/DAY1/index.js
+++ javascript/AIFEEL_NODE/DAY1/index.js
@@ -1,3 +1,7 @@
+// 프로젝트에서 node package를 참조합니다
+// if you want to use node package use require command
+const moment = require("moment");
+
 console.log("터미널 창에 로그 정보 기록합니다");
 
 console.log("현재 날짜 시간 정보를 출력합니다", Date.now());
