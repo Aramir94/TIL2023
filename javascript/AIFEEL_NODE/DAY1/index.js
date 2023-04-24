96fddcc7adc71f3769db57934ec8e2b994308694
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2023-04-24 21:22:55 +0900

    날짜는 짜릿해

--- javascript/AIFEEL_NODE/DAY1/index.js
+++ javascript/AIFEEL_NODE/DAY1/index.js
@@ -5,3 +5,8 @@ const moment = require("moment");
 console.log("터미널 창에 로그 정보 기록합니다");
 
 console.log("현재 날짜 시간 정보를 출력합니다", Date.now());
+
+console.log(
+  "현재 날짜 시간 정보를 출력합니다",
+  moment(Date.now()).format("YYYY-MM-DD HH:mm:ss")
+);
